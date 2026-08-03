7b63be653bdfb3e8ae987b5cb60642005c4ea23c
peymanba273-design <peymanba273@gmail.com>
2026-08-04 02:43:07 +0330

    Create Logo.jsx

---
+++ src/Logo.jsx
@@ -0,0 +1,25 @@
+import "./styles/logo.css";
+
+
+function Logo(){
+
+  return(
+
+    <div className="logo-container">
+
+      <div className="logo-b">
+        B
+      </div>
+
+      <h2>
+        Barzanjeh Gold
+      </h2>
+
+    </div>
+
+  );
+
+}
+
+
+export default Logo;
