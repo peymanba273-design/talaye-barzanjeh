9a9de3670a4f4bb820e075896691c0831ac074f3
peymanba273-design <peymanba273@gmail.com>
2026-08-04 03:28:06 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,54 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./src/pages/Home";
-import Market from "./src/pages/Market";
-import WalletPage from "./src/pages/WalletPage";
-import Settings from "./src/pages/Settings";
-
-import Navbar from "./src/Navbar";
+import Home from "./src/Home";
 
 
 function App(){
 
   return (
 
-    <BrowserRouter>
+    <>
 
+      <Home />
 
-      <Routes>
-
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-
-        <Route
-          path="/market"
-          element={<Market />}
-        />
-
-
-        <Route
-          path="/wallet"
-          element={<WalletPage />}
-        />
-
-
-        <Route
-          path="/settings"
-          element={<Settings />}
-        />
-
-
-      </Routes>
-
-
-      <Navbar />
-
-
-    </BrowserRouter>
+    </>
 
   );
 
