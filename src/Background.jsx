7378ea7c777ef2e7766f71aabf031457e6c4c539
peymanba273-design <peymanba273@gmail.com>
2026-08-04 03:11:45 +0330

    Create Background.jsx

---
+++ src/Background.jsx
@@ -0,0 +1,35 @@
+import "./styles/background.css";
+
+
+function Background(){
+
+  return (
+
+    <div className="gold-background">
+
+      <div className="light light-1"></div>
+
+      <div className="light light-2"></div>
+
+      <div className="light light-3"></div>
+
+
+      <div className="particles">
+
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+
+      </div>
+
+
+    </div>
+
+  );
+
+}
+
+
+export default Background;
