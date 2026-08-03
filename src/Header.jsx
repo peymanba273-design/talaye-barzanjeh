a6a84a4e5393512d46eefe908114f526d074a768
peymanba273-design <peymanba273@gmail.com>
2026-08-04 01:47:25 +0330

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,25 @@
+import "./styles/header.css";
+
 function Header() {
   return (
     <header className="header">
+
       <div className="logo-box">
+
         <div className="logo-3d">
           ✦ B ✦
         </div>
 
-        <h1>Barzanjeh Gold</h1>
+        <h1>
+          Barzanjeh Gold
+        </h1>
 
         <p>
           سرمایه‌گذاری هوشمند روی طلا
         </p>
+
       </div>
+
     </header>
   );
 }
