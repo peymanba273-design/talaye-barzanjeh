35da835ba54a6b01bc175c5aa8eaba206aa60866
peymanba273-design <peymanba273@gmail.com>
2026-08-04 01:40:39 +0330

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import Header from "../Header";
+import PriceCards from "../PriceCards";
+
+function Home(){
+
+  return(
+    <main>
+
+      <Header />
+
+      <PriceCards />
+
+    </main>
+  );
+
+}
+
+export default Home;
