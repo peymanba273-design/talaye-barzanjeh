0229877261e34199974e1a097f1b3a352cebb1ae
peymanba273-design <peymanba273@gmail.com>
2026-08-04 01:48:29 +0330

    Update PriceCards.jsx

--- src/PriceCards.jsx
+++ src/PriceCards.jsx
@@ -1,41 +1,47 @@
-function PriceCards() {
+import "./styles/price.css";
+
+function PriceCards(){
 
   const prices = [
     {
-      name: "طلای آبشده",
-      icon: "🟨",
-      price: "75,000,000",
-      change: "+1.2%",
-      up: true
+      name:"طلای آبشده",
+      icon:"🟨",
+      price:"75,000,000",
+      change:"+1.2%",
+      up:true
     },
     {
-      name: "اونس جهانی",
-      icon: "🌍",
-      price: "3,350$",
-      change: "-0.4%",
-      up: false
+      name:"اونس جهانی",
+      icon:"🌍",
+      price:"3,350$",
+      change:"-0.4%",
+      up:false
     },
     {
-      name: "تتر",
-      icon: "💵",
-      price: "95,000",
-      change: "+0.8%",
-      up: true
+      name:"تتر",
+      icon:"💵",
+      price:"95,000",
+      change:"+0.8%",
+      up:true
     }
   ];
 
 
-  return (
+  return(
+
     <section className="prices">
 
       {prices.map((item,index)=>(
+
         <div className="price-card" key={index}>
 
           <div className="icon">
             {item.icon}
           </div>
 
-          <h3>{item.name}</h3>
+          <h3>
+            {item.name}
+          </h3>
 
           <strong>
             {item.price}
@@ -46,10 +52,13 @@ function PriceCards() {
           </span>
 
         </div>
+
       ))}
 
     </section>
+
   );
+
 }
 
 export default PriceCards;
