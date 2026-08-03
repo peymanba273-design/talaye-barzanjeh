bf1a873c65fff1f87e04021b62a5908d3c637a39
peymanba273-design <peymanba273@gmail.com>
2026-08-04 02:18:38 +0330

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,40 @@
+import "./styles/navbar.css";
+
+
+function Navbar(){
+
+  return (
+
+    <nav className="navbar">
+
+      <div>
+        🏠
+        <span>خانه</span>
+      </div>
+
+
+      <div>
+        📈
+        <span>بازار</span>
+      </div>
+
+
+      <div>
+        🪙
+        <span>کیف طلا</span>
+      </div>
+
+
+      <div>
+        ⚙️
+        <span>تنظیمات</span>
+      </div>
+
+    </nav>
+
+  );
+
+}
+
+
+export default Navbar;
