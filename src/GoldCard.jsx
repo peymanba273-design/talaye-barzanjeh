14c18536d7034720c5c5aab18f56f37fcd30d133
peymanba273-design <peymanba273@gmail.com>
2026-08-04 02:48:41 +0330

    Create GoldCard.jsx

---
+++ src/GoldCard.jsx
@@ -0,0 +1,26 @@
+import "./styles/goldcard.css";
+
+
+function GoldCard(){
+
+  return (
+
+    <div className="gold-card">
+
+      <h3>
+        طلای برزنجه
+      </h3>
+
+
+      <p>
+        سرمایه‌گذاری هوشمند روی طلا
+      </p>
+
+    </div>
+
+  );
+
+}
+
+
+export default GoldCard;
