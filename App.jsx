8480d172570abf54ea69a9ca4e5f6df8f020f254
peymanba273-design <peymanba273@gmail.com>
2026-08-04 02:39:00 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import Home from "./src/pages/Home";
 import Market from "./src/pages/Market";
 import WalletPage from "./src/pages/WalletPage";
 import Settings from "./src/pages/Settings";
+
+import Navbar from "./src/Navbar";
 
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
   return (
 
     <BrowserRouter>
+
 
       <Routes>
 
@@ -41,6 +43,10 @@ function App(){
 
 
       </Routes>
+
+
+      <Navbar />
+
 
     </BrowserRouter>
 
