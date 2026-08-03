4052417a31aeea5f3bcd3540cf59000d391dd436
peymanba273-design <peymanba273@gmail.com>
2026-08-04 01:50:44 +0330

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import Home from "./src/pages/Home";
+
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
+
   <React.StrictMode>
     <Home />
   </React.StrictMode>
+
 );
