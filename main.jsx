e898df5dfb50635e7c9cbb86aaaa4e794e6f6710
peymanba273-design <peymanba273@gmail.com>
2026-08-04 14:00:41 +0330

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+function App(){
 
-import "./style.css";
+  return (
+    <div style={{
+      background:"#000",
+      color:"#d4af37",
+      minHeight:"100vh",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      fontSize:"40px"
+    }}>
+      Barzanjeh Gold
+    </div>
+  );
+
+}
 
 
 ReactDOM
 .createRoot(document.getElementById("root"))
 .render(
-
   <React.StrictMode>
-
     <App />
-
   </React.StrictMode>
-
 );
