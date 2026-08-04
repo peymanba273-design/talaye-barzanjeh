9ecb95461639e833f6ebd694b10a8684a0a8b565
peymanba273-design <peymanba273@gmail.com>
2026-08-04 04:03:17 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,75 +1,103 @@
 import { useState } from "react";
 
+import Layout from "./Layout.jsx";
+
 import Login from "./Login.jsx";
 import Register from "./Register.jsx";
 
 import Home from "./src/Home.jsx";
-import Navbar from "./Navbar.jsx";
-
+import Market from "./src/Market.jsx";
+import WalletPage from "./src/WalletPage.jsx";
+import Profile from "./Profile.jsx";
 
 function App(){
 
-  const [logged,setLogged] = useState(false);
+  const [logged,setLogged]=useState(false);
 
-  const [page,setPage] = useState("login");
+  const [authPage,setAuthPage]=useState("login");
+
+  const [page,setPage]=useState("home");
 
 
 
   if(!logged){
 
+    if(authPage==="register"){
 
-    if(page==="register"){
-
-
-      return (
+      return(
 
         <Register
 
-        setLogged={setLogged}
+          setLogged={setLogged}
+
+          setAuthPage={setAuthPage}
 
         />
 
       );
 
-
     }
 
 
 
-    return (
+    return(
 
       <Login
 
-      setLogged={setLogged}
+        setLogged={setLogged}
 
-      setPage={setPage}
+        setAuthPage={setAuthPage}
 
       />
 
     );
 
+  }
+
+
+
+  function renderPage(){
+
+    switch(page){
+
+      case "market":
+
+        return <Market />;
+
+      case "wallet":
+
+        return <WalletPage />;
+
+      case "profile":
+
+        return <Profile />;
+
+      default:
+
+        return <Home />;
+
+    }
 
   }
 
 
 
-  return (
+  return(
 
-    <div className="app">
+    <Layout
 
+      page={page}
 
-      <Home />
+      setPage={setPage}
 
+    >
 
-      <Navbar />
+      {renderPage()}
 
-
-    </div>
+    </Layout>
 
   );
 
-
 }
-
 
 export default App;
