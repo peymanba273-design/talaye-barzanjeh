07a9adbc03a13c0f4f5715cebac32d88cf8a6363
peymanba273-design <peymanba273@gmail.com>
2026-08-04 03:33:33 +0330

    Update Market.jsx

--- src/Market.jsx
+++ src/Market.jsx
@@ -1,23 +1,81 @@
-import Chart from "../Chart";
-import PriceCards from "../PriceCards";
-import "./styles/market.css";
+import "./market.css";
 
 
 function Market(){
 
+  const marketData = [
+
+    {
+      name:"طلای ۱۸ عیار",
+      price:"۷,۸۵۰,۰۰۰ تومان",
+      change:"+۲.۴٪"
+    },
+
+    {
+      name:"اونس جهانی",
+      price:"۳,۳۵۰ دلار",
+      change:"+۰.۸٪"
+    },
+
+    {
+      name:"دلار",
+      price:"۹۵,۰۰۰ تومان",
+      change:"+۱.۲٪"
+    },
+
+    {
+      name:"سکه",
+      price:"۸۶,۰۰۰,۰۰۰ تومان",
+      change:"+۱.۷٪"
+    }
+
+  ];
+
+
   return (
 
-    <section className="market">
+    <section className="market-page">
+
 
       <h1>
         بازار طلا
       </h1>
 
 
-      <PriceCards />
+      <div className="market-grid">
 
 
-      <Chart />
+        {
+          marketData.map((item,index)=>(
+
+            <div 
+            className="market-card"
+            key={index}
+            >
+
+
+              <h3>
+                {item.name}
+              </h3>
+
+
+              <strong>
+                {item.price}
+              </strong>
+
+
+              <span>
+                {item.change}
+              </span>
+
+
+            </div>
+
+          ))
+        }
+
+
+      </div>
 
 
     </section>
