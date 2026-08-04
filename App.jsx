4c2a4f6ac357a3a8b2b6055172a2f1b1df9c6ce6
peymanba273-design <peymanba273@gmail.com>
2026-08-04 13:03:47 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,89 +1,74 @@
 import { useState } from "react";
 
-import Home from "./Home.jsx";
-import Market from "./Market.jsx";
-import WalletPage from "./WalletPage.jsx";
+import Home from "./src/Home.jsx";
+import Market from "./src/Market.jsx";
+import WalletPage from "./src/WalletPage.jsx";
+
+import Header from "./Header.jsx";
+import Navbar from "./Navbar.jsx";
+
 import Profile from "./Profile.jsx";
 import Notifications from "./Notifications.jsx";
 import Transactions from "./Transactions.jsx";
 import News from "./News.jsx";
 
-import Navbar from "./Navbar.jsx";
-import Header from "./Header.jsx";
+function App() {
 
+  const [page, setPage] = useState("home");
 
-function App(){
+  const renderPage = () => {
 
+    switch (page) {
 
-  const [page,setPage]=useState("home");
-
-
-
-  function renderPage(){
-
-
-    switch(page){
-
+      case "home":
+        return <Home />;
 
       case "market":
         return <Market />;
 
-
       case "wallet":
         return <WalletPage />;
-
 
       case "profile":
         return <Profile />;
 
-
       case "notifications":
         return <Notifications />;
-
 
       case "transactions":
         return <Transactions />;
 
-
       case "news":
         return <News />;
-
 
       default:
         return <Home />;
 
     }
 
-  }
+  };
 
+  return (
 
-
-  return(
-
-    <div>
-
+    <div className="app">
 
       <Header />
 
+      <main>
 
-      {renderPage()}
+        {renderPage()}
 
+      </main>
 
       <Navbar
-
-      page={page}
-
-      setPage={setPage}
-
+        page={page}
+        setPage={setPage}
       />
-
 
     </div>
 
   );
 
 }
-
-
 
 export default App;
