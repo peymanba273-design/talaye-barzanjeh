47f3e3484b3eb3bf27672679f4c7c455b417b716
peymanba273-design <peymanba273@gmail.com>
2026-08-04 03:40:09 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,18 +1,60 @@
+import { useState } from "react";
+
 import Home from "./src/Home.jsx";
+import Market from "./src/Market.jsx";
+import WalletPage from "./src/WalletPage.jsx";
+import Settings from "./src/Settings.jsx";
+
 import Navbar from "./Navbar.jsx";
 
 
 function App(){
+
+  const [page,setPage] = useState("home");
+
+
+  function renderPage(){
+
+    if(page==="market"){
+
+      return <Market />;
+
+    }
+
+
+    if(page==="wallet"){
+
+      return <WalletPage />;
+
+    }
+
+
+    if(page==="settings"){
+
+      return <Settings />;
+
+    }
+
+
+    return <Home />;
+
+  }
+
+
 
   return (
 
     <div className="app">
 
 
-      <Home />
+      {renderPage()}
 
 
-      <Navbar />
+
+      <Navbar 
+        setPage={setPage}
+        page={page}
+      />
 
 
     </div>
