c29e2c80045cb3498497b7be1bdf9bac2280658e
peymanba273-design <peymanba273@gmail.com>
2026-08-04 13:24:29 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,71 +1,21 @@
-import { useState } from "react";
-
-import Header from "./Header.jsx";
-import Navbar from "./Navbar.jsx";
-
-import Home from "./src/Home.jsx";
-import Market from "./src/Market.jsx";
-import WalletPage from "./src/WalletPage.jsx";
-
-import Profile from "./Profile.jsx";
-import Notifications from "./Notifications.jsx";
-import Transactions from "./Transactions.jsx";
-import News from "./News.jsx";
-
-
-function App() {
-
-  const [page, setPage] = useState("home");
-
-
-  const pages = {
-
-    home: <Home />,
-
-    market: <Market />,
-
-    wallet: <WalletPage />,
-
-    profile: <Profile />,
-
-    notifications: <Notifications />,
-
-    transactions: <Transactions />,
-
-    news: <News />
-
-  };
-
+function App(){
 
   return (
 
-    <div className="app">
+    <div style={{
+      color:"white",
+      background:"black",
+      minHeight:"100vh",
+      padding:"50px",
+      fontSize:"40px"
+    }}>
 
-
-      <Header />
-
-
-      <div className="content">
-
-        {pages[page]}
-
-      </div>
-
-
-      <Navbar
-
-        page={page}
-
-        setPage={setPage}
-
-      />
-
+      Barzanjeh Gold Test
 
     </div>
 
   );
 
 }
-
 
 export default App;
