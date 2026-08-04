572a658ef70aae5c3402203958632421c770681f
peymanba273-design <peymanba273@gmail.com>
2026-08-04 13:32:28 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react],
 
-  base: "/talaye-barzanjeh/"
+  plugins: [
+    react()
+  ],
+
+  base: "/talaye-barzanjeh/",
+
+  build: {
+    outDir: "dist"
+  }
+
 });
