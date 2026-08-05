f376503b8c5d8271b39dfb592c9f97d854586e5c
peymanba273-design <peymanba273@gmail.com>
2026-08-05 23:09:37 +0330

    Update priceFetcher.js

--- backend/priceFetcher.js
+++ backend/priceFetcher.js
@@ -1,1 +1,106 @@
 
+import fs from "fs";
+import path from "path";
+
+
+const cachePath = path.join(
+  process.cwd(),
+  "cache.json"
+);
+
+
+
+function readCache(){
+
+  try{
+
+    const data = fs.readFileSync(
+      cachePath,
+      "utf-8"
+    );
+
+    return JSON.parse(data);
+
+  }
+
+  catch(error){
+
+    return {
+
+      gold18:0,
+      gold24:0,
+      coin:0,
+      dollar:0,
+      ounce:0,
+      updated:""
+
+    };
+
+  }
+
+}
+
+
+
+
+function saveCache(data){
+
+  fs.writeFileSync(
+
+    cachePath,
+
+    JSON.stringify(
+      data,
+      null,
+      2
+    )
+
+  );
+
+}
+
+
+
+
+async function fetchPrices(){
+
+
+  /*
+    اینجا بعداً API واقعی قرار می‌گیرد
+
+    مثال:
+
+    API طلا
+    یا سایت ایرانی
+    یا ربات تلگرام
+
+  */
+
+
+  const oldData = readCache();
+
+
+
+  const newData={
+
+    ...oldData,
+
+    updated:
+    new Date().toISOString()
+
+  };
+
+
+
+  saveCache(newData);
+
+
+
+  return newData;
+
+
+}
+
+
+
+export default fetchPrices;
