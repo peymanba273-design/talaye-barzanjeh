62b7f907951e9390edfa7803ae5518a51140e65c
peymanba273-design <peymanba273@gmail.com>
2026-08-05 19:32:46 +0330

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 import Home from "./Home.jsx";
 import Market from "./Market.jsx";
 import WalletPage from "./WalletPage.jsx";
+import Profile from "./Profile.jsx";
+import Transactions from "./Transactions.jsx";
+import News from "./News.jsx";
+import Notifications from "./Notifications.jsx";
 
-import Layout from "../Layout.jsx";
+import Layout from "./Layout.jsx";
 
 
 function App(){
 
-  const [page,setPage] = useState("home");
+  const [page,setPage]=useState("home");
 
 
   function renderPage(){
@@ -24,7 +28,24 @@ function App(){
         return <WalletPage />;
 
 
+      case "profile":
+        return <Profile />;
+
+
+      case "transactions":
+        return <Transactions />;
+
+
+      case "news":
+        return <News />;
+
+
+      case "notifications":
+        return <Notifications />;
+
+
       default:
+
         return <Home />;
 
     }
@@ -43,7 +64,6 @@ function App(){
     >
 
       {renderPage()}
-
 
     </Layout>
 
