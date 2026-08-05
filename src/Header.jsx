bfd40a48e11df069ec3f01cfa63f18ba664a91d3
peymanba273-design <peymanba273@gmail.com>
2026-08-05 04:09:35 +0330

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,65 @@
-import Logo from "./Logo.jsx";
 import "./header.css";
+import { useEffect, useState } from "react";
 
 
 function Header(){
 
+  const [time,setTime] = useState(new Date());
+
+
+  useEffect(()=>{
+
+    const timer = setInterval(()=>{
+
+      setTime(new Date());
+
+    },1000);
+
+
+    return ()=>clearInterval(timer);
+
+  },[]);
+
+
+
   return(
 
-    <header className="gold-header">
+    <header className="top-header">
 
 
-      <button className="header-action">
+      <div className="header-time">
+
+        <span>
+          {time.toLocaleDateString("fa-IR")}
+        </span>
+
+
+        <span>
+          {time.toLocaleTimeString("fa-IR",{
+            hour:"2-digit",
+            minute:"2-digit"
+          })}
+        </span>
+
+
+      </div>
+
+
+
+      <button className="settings-btn">
+
         ⚙
+
       </button>
 
 
-      <div className="header-logo">
 
-        <Logo />
+      <h1 className="header-title">
 
-      </div>
+        Barzanjeh Gold
+
+      </h1>
+
 
 
     </header>
