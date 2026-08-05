5b9244f9babd597b78c14221e3d62eb649b07e74
peymanba273-design <peymanba273@gmail.com>
2026-08-05 23:11:00 +0330

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,81 @@
 
+import express from "express";
+import cors from "cors";
+import fetchPrices from "./priceFetcher.js";
+
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+
+app.get("/", (req,res)=>{
+
+
+  res.json({
+
+    status:"Barzanjeh Gold API is running"
+
+  });
+
+
+});
+
+
+
+
+
+app.get("/api/prices", async (req,res)=>{
+
+
+  try{
+
+
+    const prices = await fetchPrices();
+
+
+    res.json(prices);
+
+
+
+  }
+
+  catch(error){
+
+
+    res.status(500).json({
+
+      error:"Price service error"
+
+    });
+
+
+  }
+
+
+
+});
+
+
+
+
+
+const PORT = 5000;
+
+
+
+app.listen(PORT,()=>{
+
+
+  console.log(
+
+    `Backend running on port ${PORT}`
+
+  );
+
+
+});
