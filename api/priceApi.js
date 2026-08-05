1919646020f9f2aaed8d4b18693d77aa3cdc6a89
peymanba273-design <peymanba273@gmail.com>
2026-08-06 00:34:43 +0330

    Update priceApi.js

--- api/priceApi.js
+++ api/priceApi.js
@@ -1,18 +1,51 @@
+const API_URL = "";
+
+
 export async function getPrices(){
+
 
   try{
 
 
-    const response = await fetch(
-      "API_ADDRESS_HERE"
-    );
+    // وقتی API واقعی پیدا شد این قسمت فعال می‌شود
+
+    if(API_URL){
 
 
-    const data =
-    await response.json();
+      const response = await fetch(API_URL);
 
 
-    return data;
+      const data = await response.json();
+
+
+      return data;
+
+
+    }
+
+
+
+    // داده موقت برای تست اتصال
+
+    return {
+
+      gold18:7950000,
+
+      gold24:10600000,
+
+      mesghal:34500000,
+
+      coin:85000000,
+
+      halfCoin:45000000,
+
+      quarterCoin:25000000,
+
+      dollar:160000,
+
+      ounce:3350
+
+    };
 
 
   }
