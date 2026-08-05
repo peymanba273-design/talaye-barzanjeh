3a5f722bc4df4c965f19c5eee93c0d876b35a1b4
peymanba273-design <peymanba273@gmail.com>
2026-08-06 00:28:20 +0330

    Update priceApi.js

--- api/priceApi.js
+++ api/priceApi.js
@@ -1,1 +1,36 @@
+export async function getPrices(){
 
+  try{
+
+
+    const response = await fetch(
+      "API_ADDRESS_HERE"
+    );
+
+
+    const data =
+    await response.json();
+
+
+    return data;
+
+
+  }
+
+
+  catch(error){
+
+
+    console.log(
+      "Price API Error:",
+      error
+    );
+
+
+    return null;
+
+
+  }
+
+
+}
